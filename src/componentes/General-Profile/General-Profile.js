86df4e7c07425c70c1858b39336db448985f418f
Logon Aluno <logonrmlocal@fiap.com.br>
2023-03-08 22:44:34 -0300

    colocando icone de invocador

--- src/componentes/General-Profile/General-Profile.js
+++ src/componentes/General-Profile/General-Profile.js
@@ -3,7 +3,7 @@ import './General-Profile.css';
 function GeneralProfile() {
     return (
         <div className="container-Genereal-Profile">
-
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtmlZskjQKHL243dq1E6kh3JLI6lqxwKr6GQ&usqp=CAU" alt=""/>
         </div>
     );
   }
